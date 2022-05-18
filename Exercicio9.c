fccbacfe9d235f0672276ff6da1ffdbac631e541
MatheusBeltrao <57968421+MatheusBeltrao@users.noreply.github.com>
2022-05-17 21:06:42 -0300

    Update Exercicio9.c

--- Exercicio9.c
+++ Exercicio9.c
@@ -5,7 +5,7 @@ int main() {
 	float matriz[3][3] = {{1,2, 3}, {4, 5, 6}, {7, 8, 9}};
 	int a, b;
 	
-	printf("Endereço das posicoes de uma matriz [3][3]:\n\n");
+	printf("o valor do endereço da matriz [3][3]:\n\n");
 	for (a=0; a<3; a++) {
 		for (b=0; b<3; b++){
 			printf("%d\n", &matriz[a][b]);
